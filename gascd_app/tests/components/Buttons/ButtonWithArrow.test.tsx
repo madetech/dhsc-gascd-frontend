eaadc14f54c6d523f506aac00d44616ce18c4cc8
sam fallowfield <sam.fallowfield1@gmail.com>
2024-12-17 11:51:22 +0000

    finished component test

--- gascd_app/tests/components/Buttons/ButtonWithArrow.test.tsx
+++ gascd_app/tests/components/Buttons/ButtonWithArrow.test.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import ArrowButton from '@/components/common/buttons/navigation/button-with-arrow/ButtonWithArrow';
 import ButtonWithArrow from '@/components/common/buttons/navigation/button-with-arrow/ButtonWithArrow';
-import { link } from 'fs';
 
 describe('ButtonWithArrow Component', () => {
   const buttonText = 'Click here';
   const buttonUrl = '/url';
 
-  it('Renders button with correct button string and href', async () => {
+  it('Renders button with correct button string and href', () => {
     render(<ButtonWithArrow buttonString={buttonText} buttonUrl={buttonUrl} />);
 
     const linkElement = screen.getByRole('button', { name: buttonText });
