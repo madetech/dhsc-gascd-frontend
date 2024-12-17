af3332d4545ce3fb7fa6979d3040b6e7ab067543
sam fallowfield <sam.fallowfield1@gmail.com>
2024-12-17 11:32:36 +0000

    refactor test

--- gascd_app/tests/components/Buttons/ButtonWithArrow.test.tsx
+++ gascd_app/tests/components/Buttons/ButtonWithArrow.test.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import ArrowButton from '@/components/common/buttons/navigation/button-with-arrow/ButtonWithArrow';
-import StandardButton from '@/components/common/buttons/functionality/standard-button/StandardButton';
-
-jest.mock('next/router', () => ({
-  useRouter: jest.fn(),
-}));
+import ButtonWithArrow from '@/components/common/buttons/navigation/button-with-arrow/ButtonWithArrow';
+import { link } from 'fs';
 
 describe('ButtonWithArrow Component', () => {
-  it('Renders button with correct button string', () => {
-    render(<ArrowButton buttonString="Click here" buttonUrl=""></ArrowButton>);
-    expect(
-      screen.getByRole('button', { name: 'Click here' })
-    ).toBeInTheDocument();
+  const buttonText = 'Click here';
+  const buttonUrl = '/url';
+
+  it('Renders button with correct button string and href', async () => {
+    render(<ButtonWithArrow buttonString={buttonText} buttonUrl={buttonUrl} />);
+
+    const linkElement = screen.getByRole('button', { name: buttonText });
+
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute('href', buttonUrl);
   });
 });
