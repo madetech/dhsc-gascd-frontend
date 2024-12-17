76da9d4b9f6423f453376fd495c5c13ce80b6d23
sam fallowfield <sam.fallowfield1@gmail.com>
2024-12-17 11:07:59 +0000

    added first test for new component

--- gascd_app/tests/components/Buttons/StandardButton.test.tsx
+++ gascd_app/tests/components/Buttons/StandardButton.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import StandardButton from '@/components/common/buttons/functionality/standard-button/StandardButton.tsx';
 
-describe('StandardButton', () => {
+describe('StandardButton Component', () => {
   it('renders with the correct button string', () => {
     render(
       <StandardButton
